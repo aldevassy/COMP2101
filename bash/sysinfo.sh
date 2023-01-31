8d5d7e8e0eac8f07219f33021f5b5225826556f1
aldevassy <200464997@student.georgianc.on.ca>
2023-01-31 18:51:04 -0500

    bashlab1

---
+++ bash/sysinfo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#To display fully-qualified domain name (FQDN)
+
+echo "Domain name: $(hostname -f)"
+
+
+#To display operating system name and version
+
+echo "Host Information:" 
+hostnamectl
+
+#To display IP addresses the machine has that are not on the 127 network
+
+echo "IP Addresses:$(ip addr | grep -v "127" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tr '\n' ' ')"
+
+#To display the the amount of space available in only the root filesystem
+
+echo "Space available: $(df -h / | tail -1 | awk '{print $4}')"
+
