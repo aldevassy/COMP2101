83621831b5e612c365e23594dedc73080c35b7b2
aldevassy <200464997@student.georgianc.on.ca>
2023-02-15 01:08:44 -0500

    lab2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-#To display fully-qualified domain name (FQDN)
+#hostname and FQDN
+hostname=$(hostname)
+fqdn=$(hostname -f)
 
-echo "Domain name: $(hostname -f)"
+#OS name and version
+os_info=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d '"' -f 2)
 
-
-#To display operating system name and version
-
-echo "Host Information:" 
-hostnamectl
+# default route IP address
+ip_address=$(ip route get 8.8.8.8 | awk 'NR==1 {print $7}')
 
 #To display IP addresses the machine has that are not on the 127 network
+#ip_address=$(ip addr | grep -v "127" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tr '\n' ' ')
 
-echo "IP Addresses:$(ip addr | grep -v "127" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tr '\n' ' ')"
+#Root filesystem free space
+root_space=$(df -h / | awk 'NR==2 { print $4 }') 
+cat <<EOF
 
-#To display the the amount of space available in only the root filesystem
+Hostname: $hostname
 
-echo "Space available: $(df -h / | tail -1 | awk '{print $4}')"
+--------------------------------------------------
+--------------------------------------------------
+Fully Qualified Domain Name: $fqdn
+Operating System: $os_info
+Default Route IP: $ip_address
+Root Filesystem Free Space: $root_space
+--------------------------------------------------
+--------------------------------------------------
+EOF
+
+
+
+
+
 
